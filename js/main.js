2ad9517b105ab19b32f0eb74658d6d075700c4e6
stringerc <stringer.c.a@gmail.com>
2025-01-06 11:08:11 -0500

    css update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+ 
+
 (function ($) {
+
+    
+
     "use strict";
+
+    
 
     // Spinner
     var spinner = function () {
@@ -76,8 +83,60 @@
                 items:2
             }
         }
+        
     });
 
     
+    
+    
 })(jQuery);
 
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.industries-carousel');
+let SliderDom = carouselDom.querySelector('.industries-carousel .list');
+let thumbnailBorderDom = document.querySelector('.industries-carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.industries-carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 4000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.industries-carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.industries-carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
